5b3ef71f76829ac2562172dc2f094917fa1cf3b1
zawadzki-dawid <lamazlo21@wp.pl>
2020-04-18 22:49:11 +0200

    Find eye in the image

--- script.py
+++ script.py
@@ -1,17 +1,34 @@
 import cv2 as cv
 import dlib
+import numpy as np
+import argparse
 from src.cameraHandler import CameraHandler
 from src.utils import markFace
 
+kernel = [
+    1, 2, 1,
+    2, 4, 2,
+    1, 2, 1
+]
+
+kernel = np.asarray(kernel)
+kernel = kernel/kernel.sum()
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True, help="Path to facial landmark predictor")
+args = vars(ap.parse_args())
+
 camera = CameraHandler()
 detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
 
 while True:
     ret, frame = camera.getFrame()
     frameLR = cv.pyrDown(frame)
     gray = camera.frameToGray(frameLR)
+    #gray = cv.filter2D(gray, -1, kernel=kernel)
     faces = detector(gray, 1)
-    markFace(frameLR, faces)
+    markFace(frameLR, gray, faces, predictor)
     camera.displayFrame('Kamera', frameLR)
     key = cv.waitKey(1) & 0xFF
     if key == ord('q'):
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
 import cv2 as cv
+import numpy as np
+
+eyeTopP = 15
+eyeSideP = 13
+eyeHeightP = 30
+eyeWidthP = 35
 
 def rectToBB(face):
     x = face.left()
@@ -7,7 +13,23 @@ def rectToBB(face):
     h = face.bottom() - y
     return (x, y, w, h)
 
-def markFace(image, faces):
-    if len(faces) > 0:
-        (x, y, w, h) = rectToBB(faces[0])
-        cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+def shapeToNp(shape, dtype="int"):
+	coords = np.zeros((shape.num_parts, 2), dtype=dtype)
+	for i in range(0, shape.num_parts):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+	return coords
+
+def markFace(frame, gray, faces, predictor):
+    facesCoords = []
+    for (i, face) in enumerate(faces):
+
+        shape = predictor(gray, face)
+        for i in range(shape.num_parts):
+            if i >= 36 and i <= 47:
+                x = shape.part(i).x
+                y = shape.part(i).y
+                cv.circle(frame, (x, y), 1, (0, 0, 255), -1)
+
+        (x, y, w, h) = rectToBB(face)
+        facesCoords.append((x, y, w, h))
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
