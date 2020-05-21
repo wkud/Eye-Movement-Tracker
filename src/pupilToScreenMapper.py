6e9b70f9273a4a8e6aff260643d175b51df64560
wkud <vopox07@gmail.com>
2020-05-21 03:17:45 +0200

    Add optimisation fix: draw only new points

--- src/pupilToScreenMapper.py
+++ src/pupilToScreenMapper.py
@@ -1,5 +1,6 @@
 import src.utils as utils
 
+
 class PupilToScreenMapper:
     def __init__(self):
         self.screenSize = utils.getMonitorSize();
@@ -8,14 +9,17 @@ class PupilToScreenMapper:
         (ex, ey, ew, eh) = eyeBox
         (px, py) = pupilPos
 
-        rpx = px - ex # Relative Pupil X - pupil position relative to eye coords
+        rpx = px - ex  # Relative Pupil X - pupil position relative to eye coords
         rpy = py - ey
 
-        scaleX = rpx * 1.0 / ew # (relative) pupilX as percent of whole width
+        scaleX = rpx * 1.0 / ew  # (relative) pupilX as percent of whole width
         scaleY = rpy * 1.0 / eh
 
         sw, sh = self.screenSize
-        screenPosX = scaleX * sw # proportinally mapped
-        screenPosY = scaleY * sh
+        x = scaleX * sw  # proportinally mapped
+        y = scaleY * sh
+
+        screenPosX = min(x, sw - 1)  # make sure, that x, y indices aren't greater than screen size
+        screenPosY = min(y, sh - 1)
 
         return (screenPosX, screenPosY)
